245bb17424473f62d13a9acc402478b37a2ad17e
Kiran jadhav <kiranjadhav9561@gmail.com>
2024-03-09 19:48:21 +0530

    added express status

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-
+import statusMonitor from 'express-status-monitor';
 const app = express()
 
+app.use(statusMonitor());
 
 app.use(cors());
 app.use(express.json({ limit: "16kb" }))
